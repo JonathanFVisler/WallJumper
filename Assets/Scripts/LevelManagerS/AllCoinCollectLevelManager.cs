555d84f130e40ecc43d0e399b9c58e21724cf94f
Jonathan Frimann Visler <levendeslot@gmail.com>
2020-02-03 10:28:19 +0100

    Upstart of LevelManager

---
+++ Assets/Scripts/LevelManagerS/AllCoinCollectLevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllCoinCollectLevelManager : MonoBehaviour, CoinCollectLevelManagerStrategy
+{
+    int score = 0;
+    float Timer = 0;
+    [SerializeField]
+    float StartTimer = 60;
+
+    public BeatDegree BeatLevel(){
+        return BeatDegree.FAIL;
+    }
+
+    public float GetTimeToBeat(){
+        return 0;
+    }
+
+    public float GetScore(){
+        return 0;
+    }
+
+    public Dictionary<string, float> GetHighScores(){
+        return new Dictionary<string, float>();
+    }
+
+    public void PlayLevel(){
+
+    }
+
+    public void CoinPickup(){
+
+    }
+
+    public int GetCoinCount(){
+        return 0;
+    }
+}
---
+++ Assets/Scripts/LevelManagerS/LevelManagerStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface LevelManagerStrategy
+{
+    BeatDegree BeatLevel();
+
+    float GetTimeToBeat();
+
+    float GetScore();
+
+    Dictionary<string,float> GetHighScores();
+
+    void PlayLevel();
+}
+
+public enum BeatDegree {
+    FAIL,
+    ONEStar,
+    TWOStar,
+    THREEStar
+};
